bf77d07e220219178699cbb4f318894bd89b12ee
Tracy Zhu <61514951+Tracyyy-Zhu@users.noreply.github.com>
2022-05-16 16:23:56 -0400

    Add files via upload

---
+++ distil_finetune.py
@@ -0,0 +1,22 @@
+import transformers
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
+
+def compute_metrics(eval_pred):
+    """Computes accuracy, f1, precision, and recall from a 
+    transformers.trainer_utils.EvalPrediction object.
+    """
+    labels = eval_pred.label_ids
+    preds = eval_pred.predictions.argmax(-1)
+
+    metrics = precision_recall_fscore_support(labels, preds)
+    met = {'accuracy': accuracy_score(labels, preds),
+           'f1': metrics[2][1],
+           'precision': metrics[0][1],
+           'recall': metrics[1][1]}
+    return met
+
+def model_init():
+    """Modle initialization"""
+    model = transformers.DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+    return model
